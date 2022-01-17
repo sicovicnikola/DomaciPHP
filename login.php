24fc9933afb5ea58ec57ee281c1c2d3bee86506b
Nikola Sicovic <sicovic.nikola@gmail.com>
2022-01-17 20:47:25 +0100

    Login stranica css i html

---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/loginForm.css">
+    
+    <title>Notes</title>
+</head>
+<body>
+
+<div class="form">
+      <div class="title">Welcome</div>
+      <div class="subtitle">Let's write your notes</div>
+      <div class="input-container ic1">
+        <input id="username" class="input" type="text" name="username" placeholder="" />
+        <div class="cut"></div>
+        <label for="username" class="placeholder">Username</label>
+      </div>
+      <div class="input-container ic2">
+        <input id="password" class="input" type="password" name="password" placeholder="" />
+        <div class="cut"></div>
+        <label for="password" class="placeholder">Password</label>
+      </div>
+      <button type="text" class="submit">Log In</button>
+    </div>
+    
+</body>
+</html>
