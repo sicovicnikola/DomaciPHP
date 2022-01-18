c75e0f0a6e4ecaa838dfcb4ed12fe45014564491
Nikola Sicovic <sicovic.nikola@gmail.com>
2022-01-18 17:54:50 +0100

    Definisane osnovne funkcije domena sa bazom

--- domain/note.php
+++ domain/note.php
@@ -17,6 +17,44 @@
     }
 
 
+    public static function getAllNotes(mysqli $connection)
+    {
+        $user_id= $_SESSION['user_id'];
+        $sql = "SELECT * FROM notes WHERE userId= $user_id";
+        return $connection->query($sql);
+    }
+
+    public static function getNoteById($id, mysqli $connection)
+    {
+        $sql = "SELECT * FROM notes WHERE id=$id";
+        $myArray = array();
+        if ($result = $connection->query($sql)) {
+
+            while ($row = $result->fetch_array(1)) {
+                $myArray[] = $row;
+            }
+        }
+        return $myArray;
+    }
+
+    public static function deleteNoteById($id, mysqli $connection)
+    {
+        $sql = "DELETE FROM notes WHERE id=$id";
+        return $connection->query($sql);
+    }
+
+    public static function addNote($title, $content, $userId, mysqli $connection)
+    {
+        $sql = "INSERT INTO notes(title,content,userId) VALUES('$title','$content','$userId')";
+        return $connection->query($sql);
+    }
+
+    public static function editNote($id, $title,$content, mysqli $connection)
+    {
+        $sql = "UPDATE notes SET title='$title', content='$content' WHERE id=$id";
+        return $connection->query($sql);
+    }
+
  }
 
 
